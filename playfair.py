134b98c6b19527b5cfcc44e5ecbea7371bb0834a
sakshi-mj <160476449+sakshi-mj@users.noreply.github.com>
2025-04-30 13:37:36 +0530

    Update playfair.py

--- playfair.py
+++ playfair.py
@@ -1,83 +1,41 @@
-def generate_matrix(key):
-    key = key.replace("J", "I").upper()
-    matrix = []
-    used_chars = set()
-    
-    for char in key:
-        if char not in used_chars and char.isalpha():
-            matrix.append(char)
-            used_chars.add(char)
-    
-    for char in "ABCDEFGHIKLMNOPQRSTUVWXYZ":  # 'J' is merged with 'I'
-        if char not in used_chars:
-            matrix.append(char)
-            used_chars.add(char)
-    
-    return [matrix[i:i+5] for i in range(0, 25, 5)]
+def mat(k):
+    k = ''.join(dict.fromkeys(k.upper().replace('J', 'I')))
+    abc = 'ABCDEFGHIKLMNOPQRSTUVWXYZ'
+    k += ''.join(c for c in abc if c not in k)
+    return [list(k[i:i+5]) for i in range(0, 25, 5)]
 
-def find_position(matrix, char):
-    for i, row in enumerate(matrix):
-        if char in row:
-            return i, row.index(char)
-    return None
+def pos(m, ch):
+    for i in range(5):
+        if ch in m[i]:
+            return i, m[i].index(ch)
 
-def process_text(text):
-    text = text.replace("J", "I").upper()
-    text = ''.join([c for c in text if c.isalpha()])
-    processed = ""
-    i = 0
-    while i < len(text):
-        if i == len(text) - 1:
-            processed += text[i] + 'X'
-            i += 1
-        elif text[i] == text[i+1]:
-            processed += text[i] + 'X'
-            i += 1
+def pairs(t):
+    t = t.upper().replace('J','I').replace(' ','')
+    i, out = 0, []
+    while i < len(t):
+        a = t[i]
+        b = t[i+1] if i+1 < len(t) and t[i] != t[i+1] else 'X'
+        out.append((a, b))
+        i += 2 if b != 'X' else 1
+    if len(out[-1]) == 1:
+        out[-1] = (out[-1][0], 'X')
+    return out
+
+def playfair(msg, key, mode='e'):
+    m = mat(key)
+    res = ''
+    for a, b in pairs(msg):
+        r1, c1 = pos(m, a)
+        r2, c2 = pos(m, b)
+        if r1 == r2:
+            res += m[r1][(c1+1)%5] + m[r2][(c2+1)%5] if mode == 'e' else m[r1][(c1-1)%5] + m[r2][(c2-1)%5]
+        elif c1 == c2:
+            res += m[(r1+1)%5][c1] + m[(r2+1)%5][c2] if mode == 'e' else m[(r1-1)%5][c1] + m[(r2-1)%5][c2]
         else:
-            processed += text[i] + text[i+1]
-            i += 2
-    return processed
+            res += m[r1][c2] + m[r2][c1]
+    return res
 
-def playfair_encrypt(text, key):
-    matrix = generate_matrix(key)
-    text = process_text(text)
-    encrypted = ""
-    
-    for i in range(0, len(text), 2):
-        row1, col1 = find_position(matrix, text[i])
-        row2, col2 = find_position(matrix, text[i+1])
-        
-        if row1 == row2:
-            encrypted += matrix[row1][(col1 + 1) % 5] + matrix[row2][(col2 + 1) % 5]
-        elif col1 == col2:
-            encrypted += matrix[(row1 + 1) % 5][col1] + matrix[(row2 + 1) % 5][col2]
-        else:
-            encrypted += matrix[row1][col2] + matrix[row2][col1]
-    
-    return encrypted
-
-def playfair_decrypt(text, key):
-    matrix = generate_matrix(key)
-    decrypted = ""
-    
-    for i in range(0, len(text), 2):
-        row1, col1 = find_position(matrix, text[i])
-        row2, col2 = find_position(matrix, text[i+1])
-        
-        if row1 == row2:
-            decrypted += matrix[row1][(col1 - 1) % 5] + matrix[row2][(col2 - 1) % 5]
-        elif col1 == col2:
-            decrypted += matrix[(row1 - 1) % 5][col1] + matrix[(row2 - 1) % 5][col2]
-        else:
-            decrypted += matrix[row1][col2] + matrix[row2][col1]
-    
-    return decrypted.replace("X", "")
-
-text = input("Enter text: ")
-key = input("Enter key: ")
-
-encrypted = playfair_encrypt(text, key)
-print("Encrypted text:", encrypted)
-
-decrypted = playfair_decrypt(encrypted, key)
-print("Decrypted text:", decrypted)
+text = input("txt: ")
+key = input("key: ")
+mode = input("m: ")
+print("output: ", playfair(text, key, mode))
